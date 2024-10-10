cdf9b360fcb3786597fcb50bb1186121c39135f7
Sebastian Barroso <sebibarrosoh@gmail.com>
2024-10-09 22:02:22 -0500

    set up default styling for board

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -6,7 +6,26 @@ const NUM_COLS = 3;
 function App() {
   return (
     <>
-      <Board num_rows={NUM_ROWS} num_cols={NUM_COLS} />
+      <div
+        style={{
+          backgroundColor: "black",
+          color: "white",
+          height: "100vh",
+          width: "100%",
+          display: "flex",
+        }}
+      >
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Board num_rows={NUM_ROWS} num_cols={NUM_COLS} />
+        </div>
+      </div>
     </>
   );
 }
--- project/src/components/Board/Board.tsx
+++ project/src/components/Board/Board.tsx
@@ -2,29 +2,27 @@ import { Button, Grid2 } from "@mui/material";
 import React from "react";
 
 interface BoardProps {
-  num_rows: number,
-  num_cols: number
+  num_rows: number;
+  num_cols: number;
 }
 
-function Board({num_rows, num_cols}: BoardProps):React.ReactNode{
-  return  (
-    <Grid2 container spacing={2} sx={{ width: '100%' }}>
+function Board({ num_rows, num_cols }: BoardProps): React.ReactNode {
+  return (
+    <Grid2 container spacing={1}>
       {Array.from(Array(num_rows * num_cols)).map((_, index) => (
-        <Grid2 size={12/num_cols} key={index}>
+        <Grid2 size={12 / num_cols} key={index}>
           <Button
             variant="outlined"
             // onClick={() => handleClick(index)}
             sx={{
-              width: '100%',
-              height: '170px',
-              fontSize: '2rem',
+              width: "100%",
+              height: "170px",
             }}
-          >
-          </Button>
+          ></Button>
         </Grid2>
       ))}
     </Grid2>
-  )
+  );
 }
 
-export default Board
+export default Board;
