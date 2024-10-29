fa59d78fdb9689102e9c5cbe8001eb859252d933
Sebastian Barroso <sebibarrosoh@gmail.com>
2024-10-28 23:22:44 -0500

    zip manifest on startup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,7 @@
 import { response, type Express, type Request, type Response } from "express";
+import path from "path";
+import fs from "fs/promises";
+import AdmZip from "adm-zip";
 
 const express = require("express");
 const app = express();
@@ -13,28 +16,65 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 const BUNGIE_API_KEY = process.env.BUNGIE_API_KEY;
+const BUNGIE_MANIFEST_API_ENDPOINT = process.env.BUNGIE_MANIFEST_API_ENDPOINT;
 
-const GET_MANIFEST_URL = "/get-manifest-shit";
-app.get(GET_MANIFEST_URL, async (req: Request, res: Response) => {
-  /**NOTE: should move to this to just do it on startup, rather than on every request leaving it here for now till we do that */
-  if (!BUNGIE_API_KEY) return res.status(500).send("BUNGIE API KEY undefined");
+async function downloadAndOpenManifest(manifestUrl: string) {
+  const response = await fetch("https://bungie.net" + manifestUrl);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch manifest: ${response.statusText}`);
+  }
 
-  const resp = await fetch(
-    "https://www.bungie.net/Platform/Destiny2/Manifest",
-    {
-      method: "GET",
-      headers: {
-        "x-api-key": BUNGIE_API_KEY,
-      },
-    }
+  const directoryName = "./manifest_data/";
+  const filePath = path.join(directoryName, "manifest.content");
+
+  // Ensure the directory exists
+  await fs.mkdir(directoryName, { recursive: true });
+
+  const fileStream = await fs.writeFile(
+    filePath,
+    await response.arrayBuffer().then((arrayBuffer) => Buffer.from(arrayBuffer))
   );
+  console.log(`Manifest saved to ${filePath}`);
 
-  return res.json(await resp.json());
-});
+  // Create a ZIP archive
+  const zipFilePath = path.join(directoryName, "manifest.zip");
+  const zip = new AdmZip();
+  zip.addLocalFile(filePath);
+  zip.writeZip(zipFilePath);
+  console.log(`Manifest zipped to ${zipFilePath}`);
 
-app.listen(port, () => {
+  // Read and parse the file
+  // const fileContents = await fs.readFile(filePath, "utf-8");
+}
+
+async function getManifestURL() {
+  if (!BUNGIE_API_KEY) throw new Error("BUNGIE API KEY undefined");
+  if (!BUNGIE_MANIFEST_API_ENDPOINT)
+    throw new Error("BUNGIE MANIFEST API ENDPOINT undefined");
+
+  const resp = await fetch(BUNGIE_MANIFEST_API_ENDPOINT, {
+    method: "GET",
+    headers: {
+      "x-api-key": BUNGIE_API_KEY,
+    },
+  });
+
+  if (!resp.ok)
+    throw new Error(`Failed to fetch bungie API: ${resp.statusText}`);
+
+  const manifestUrl: string = await resp
+    .json()
+    .then((response) => response.Response.mobileWorldContentPaths.en);
+
+  return manifestUrl;
+}
+
+app.listen(port, async () => {
+  // get manifest URL
+  const manifestUrl = await getManifestURL();
+
+  // download manifest
+  await downloadAndOpenManifest(manifestUrl);
+
   console.log(`[server]: Server is running at http://localhost:${port}`);
-  console.log(
-    `[server]: Get Manifest here: http://localhost:${port}${GET_MANIFEST_URL}`
-  );
 });
