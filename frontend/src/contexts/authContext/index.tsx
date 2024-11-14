a715e7687a6a921ca32ed88772271a21f48634ec
Sebastian Barroso <sebibarrosoh@gmail.com>
2024-11-14 12:19:18 -0600

    add auth

---
+++ frontend/src/contexts/authContext/index.tsx
@@ -0,0 +1,53 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "../../firebase/firebase";
+
+interface AuthContextType {
+  currentUser: User | null;
+  userLoggedIn: boolean;
+  loading: boolean;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider(children: []) {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user || null);
+      setUserLoggedIn(!!user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  // async function initializeUser(user) {
+  //   if (user) {
+  //     setCurrentUser({ ...user });
+  //     setUserLoggedIn(true);
+  //   } else {
+  //     setCurrentUser(null);
+  //     setUserLoggedIn(false);
+  //   }
+  //   setLoading(false);
+  // }
+
+  const value = {
+    currentUser,
+    userLoggedIn,
+    loading,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
---
+++ frontend/src/firebase/auth.ts
@@ -0,0 +1,53 @@
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  updatePassword,
+} from "firebase/auth";
+import { auth } from "./firebase";
+import { GoogleAuthProvider } from "firebase/auth/web-extension";
+
+export const doCreateUserWithEmailAndPassword = async (
+  email: string,
+  password: string
+) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const doSignInWithEmailAndPassword = async (
+  email: string,
+  password: string
+) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+export const doSignInWithGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  const result = await signInWithPopup(auth, provider);
+
+  return result;
+};
+
+export const doSignOut = () => {
+  return auth.signOut();
+};
+
+export const doPasswordReset = (email: string) => {
+  return sendPasswordResetEmail(auth, email);
+};
+
+export const doPasswordChange = (password: string) => {
+  if (!auth.currentUser)
+    return Promise.reject("No user is currently signed in.");
+  return updatePassword(auth.currentUser, password);
+};
+
+export const doSendEmailVerification = () => {
+  if (!auth.currentUser)
+    return Promise.reject("No user is currently signed in.");
+  return sendEmailVerification(auth.currentUser, {
+    url: `${window.location.origin}/home`,
+  });
+};
